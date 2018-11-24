10a5d9c8f0320474037f5555ce95a94d61627561
Hiroki Takayasu <hiroki.kana@gmail.com>
2018-11-24 19:22:47 +0900

    modify .travis.yml Python version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requires = ['pyyaml']
 setup(
     name='paspas',
-    version='0.0.1',
+    version='0.0.2',
     packages=['paspas'],
     description='Simple password generator for CLI',
     url='https://github.com/hirokikana/paspas',
