97ff8226307a8586a9b463538b6a02a7c2489a96
Hiroki Takayasu <hiroki.kana@gmail.com>
2018-11-24 19:16:58 +0900

    Python 3 compatible

--- paspas/generator.py
+++ paspas/generator.py
@@ -3,13 +3,13 @@ import base64
 
 def _char_filter(password, unavailable_char):
     for char in unavailable_char:
-        password = password.replace(char, '')
+        password = password.replace(char.encode(),''.encode())
     return password
     
 def generate(site, user, master_password, max_length=0, unavailable_char=''):
-    password = _char_filter(base64.b64encode(hashlib.sha512(site + user + master_password).hexdigest()), unavailable_char)
+    password = _char_filter(base64.b64encode(hashlib.sha512((site + user + master_password).encode()).hexdigest().encode()), unavailable_char)
     if max_length == 0:
-        return password
+        return password.decode()
     else:
-        return password[0:max_length]
+        return password[0:max_length].decode()
         
