8257688e18e5040cf3d4dca7760447ce863f85cb
Hiroki Takayasu <hiroki.kana@gmail.com>
2018-11-25 08:26:13 +0900

    release v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 requires = ['pyyaml']
 setup(
     name='paspas',
-    version='0.0.2',
+    version='0.0.3',
     packages=['paspas'],
     description='Simple password generator for CLI',
     url='https://github.com/hirokikana/paspas',
